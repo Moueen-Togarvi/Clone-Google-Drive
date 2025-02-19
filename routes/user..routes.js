1cf58c3ab8f39a55cd2d5a4b996697bc48da581d
Moueen Togarvi <moueenuddintogarvi41@gmail.com>
2025-02-19 20:52:33 +0500

    Add user routes and integrate with main application

---
+++ routes/user..routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/test', (req, res)=> {
+    res.send('User test route');
+}
+)
+
+
+module.exports = router;
