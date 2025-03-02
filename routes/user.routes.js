c89d3e87bc6a6830143cdc92b4e82dc467158321
Moueen Togarvi <moueenuddintogarvi41@gmail.com>
2025-03-02 22:34:34 +0500

    Add lodash function aliases for improved modularity

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
 
 
 
 router.get('/register', (req, res)=> {
-    res.send('User test route');
+    res.render('register');
 })
 
-router.post('/register', (req, res))
+router.post('/register',
+    
+    body('emial').trim().isEmail().isLength({min: 8}),
+    body('password').trim().isLength({min: 6}),
+    body('username').trim().isLength({min: 3}),
+
+    (req, res)=> {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array(),
+            message: 'Invalid data'
+        });
+   }    
+   console.log(req.body);   
+    
+    res.send('User registered');
+    res.send(errors);
+} )
 
 
 module.exports = router;
