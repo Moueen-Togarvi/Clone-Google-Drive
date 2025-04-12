2be19d1d7666d0f0eab71262a4dcae2ec0626f2c
Moueen Togarvi <moueenuddintogarvi41@gmail.com>
2025-04-13 02:26:07 +0500

    Implement user authentication with JWT and update login form action

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,21 +3,18 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 router.get('/register', (req, res)=> {
     res.render('register');
-})
-
-
+});
 
 router.post('/register',
 
     body('email').trim().isEmail(),
     body('password').trim().isLength({ min: 5 }),
     body('username').trim().isLength({ min: 3 }),
-
-    
     async (req, res)=> {
 
         const errors = validationResult(req)
@@ -41,6 +38,11 @@ router.post('/register',
 })
 
 
+
+
+
+
+
 router.get('/login', (req, res)=> {
     res.render('login');
 })
@@ -51,37 +53,49 @@ router.post('/login',
     body('email').trim().isEmail(),
     body('password').trim().isLength({ min: 5 }),
     
-    async (req, res)=> {
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array(),
                 message: 'Invalid data'
+            });
+        }     
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email: email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Username or password is incorrect'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        // const isMatch = password === user.password;
+        // const isMatch = await bcrypt.compare(password, user.password);
+
+
+
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Username or password is incorrect'
+            });
+        }
+
+        const token = jwt.sign({ 
+            userid: user._id,
+            email: user.email,
+            username: user.username
+        }, process.env.JWT_SECRET);
+
+        res.json({
+            message: 'Login successful',
+            token: token
         });
-   }     
-    const { email, password } = req.body;
-
-    const user = await userModel.findOne({
-         email: email
-
-     });
-
-    if(!user){
-        return res.status(400).json({
-            message: 'Username or password is incorrect'
-        })
     }
+);
 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if(!isMatch){
-        return res.status(400).json({
-            message: 'Username or password is incorrect'
-        })
-    }
-
-    res.json(user);
-})
-
-
+// Exporting the router
 module.exports = router;
